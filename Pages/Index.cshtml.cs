d04cd0b026af6075b2dff212b731171ca23fac88
M473U52 <msokolowski62@gmail.com>
2023-03-27 18:14:43 +0200

    Dodaj pliki projektów.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using FizzBuzzWeb.Forms;
+
+namespace FizzBuzzWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public FizzBuzzForm FizzBuzz { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        public string Text { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+           
+        }
+        public IActionResult OnPost()
+        {
+            if (FizzBuzz.Number % 3 == 0 & FizzBuzz.Number % 5==0)
+            {
+                Text = "FizzBuzz";
+            }
+            else if (FizzBuzz.Number % 3 == 0)
+            {
+                Text = "Fizz";
+            }
+            else if (FizzBuzz.Number % 5 == 0)
+            {
+                Text = "Buzz";
+            }
+            else
+            {
+                Text = "Liczba: " + FizzBuzz.Number + " nie spelnia kryteriow FizzBuzz";
+            }
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+
+    }
+}
